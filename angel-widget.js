a6674c2b5c295eee782c4748691e7d24e02d1f71
Adel <adel.angel2026@gmail.com>
2026-08-22 23:19:01 +0300

    Update angel-widget.js

--- angel-widget.js
+++ angel-widget.js
@@ -26,7 +26,7 @@ var knowledgeBase = [
 
 // === НАСТРОЙКИ ПОДКЛЮЧЕНИЯ К OLLAMA ===
 // ВНИМАНИЕ: Для работы в браузере с Vercel нужен HTTPS или туннель (ngrok/localtunnel)!
-var OLLAMA_HOST = 'https://fa3825c8aae5e551-104-28-230-248.serveousercontent.com';  
+var OLLAMA_HOST = 'https://e2fff6e27bb982dd-95-32-184-49.serveousercontent.com';  
 var MODEL_NAME = 'qwen2.5:0.5b';
 
 // === ПОИСК В БАЗЕ ЗНАНИЙ ===
